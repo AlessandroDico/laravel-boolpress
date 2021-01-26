c533b8a60100dc221a4438270db85fa97cd7a69a
Ale <alessandrochb@gmail.com>
2021-01-26 19:17:43 +0100

    creato nuovo file layout + creato homecontroller admin

---
+++ app/Http/Controllers/Admin/HomeController.php
@@ -0,0 +1,15 @@
+<?php
+
+// HOMECONTROLLER LATO ADMIN
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return view('admin.home');
+    }
+}
