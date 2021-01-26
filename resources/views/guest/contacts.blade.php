0f31541505735a12890f66b283d94b81d6ba7a1b
Ale <alessandrochb@gmail.com>
2021-01-26 19:17:12 +0100

    separato le view

---
+++ resources/views/guest/contacts.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>
+                    contatti
+                </h1>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1>
+                    homepage
+                </h1>
+            </div>
+        </div>
+    </div>
+@endsection
