17026c2883b9ce5eda0012702c34a9f916cf1230
Ale <alessandrochb@gmail.com>
2021-01-27 17:33:06 +0100

    creata migration per foreign e compilata

---
+++ database/migrations/2021_01_27_161406_update_posts_table_with_foreign.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdatePostsTableWithForeign extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->unsignedBigInteger('category_id')->nullable()->after('id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            // drop foreign vuole il nome della tabella + nome colonna + foreign separati da underscore
+            $table->dropForeign('posts_category_id_foreign');
+            $table->dropColumn('category_id');
+        });
+    }
+}
