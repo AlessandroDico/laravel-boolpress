caceec3dc6121bab0f2726210ea4f18335269ce9
Ale <alessandrochb@gmail.com>
2021-02-01 21:10:32 +0100

    creato nel middleware controllo per api token dello user

--- app/Http/Middleware/CheckApiToken.php
+++ app/Http/Middleware/CheckApiToken.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\User;
 
 class CheckApiToken
 {
@@ -15,7 +16,27 @@ class CheckApiToken
      */
     public function handle($request, Closure $next)
     {
-        
+        $authorizationToken = $request->header('authorization');
+        // dd($request->header('authorization'));
+        // verifico se mi passa un qualsiasi valore
+        if (empty($authorizationToken)) {
+            return response()->json([
+                'succes' => false,
+                'error' => 'missed or invalid api_token',
+            ]);
+        };
+
+        $finalApiToken = subStr($authorizationToken, 7);
+        $tokenExist = User::where('api_token', $finalApiToken )->first();
+
+        // verifico se mi passa un valore che esiste nel DB
+        if (!$tokenExist) {
+            return response()->json([
+                'succes' => false,
+                'error' => 'missed or invalid api_token',
+            ]);
+        };
+        // dd($finalApiToken);
         return $next($request);
     }
 }
