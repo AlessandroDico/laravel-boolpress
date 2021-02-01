7de32a8b7b0abd86ad8c76c1e178083eed2f4652
Ale <alessandrochb@gmail.com>
2021-02-01 16:25:38 +0100

    creato Api\PostController + funzione che ritorna una response +dati json

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Post;
+
+class PostController extends Controller
+{
+    public function index() {
+        $data = Post::all();
+        // dd($data);
+        return response()->json([
+            'succes' => true,
+            'response' => $data,
+        ]);
+    }
+}
